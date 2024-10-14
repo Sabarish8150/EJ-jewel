5d9bc31b6288858d1647b4dbce7045aaf28747c4
Sabarish-K-S <120076169+Sabarish8150@users.noreply.github.com>
2024-10-14 17:26:41 +0530

    .

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def main():
             else:
                 st.write("Could not find 'Total' or 'Gold Weight' in the image.")
     else:
-        st.write("Please upload an image to begin.")
+        st.write("Please upload an image to beginlhkikljoi.")
 
 if __name__ == "__main__":
     main()
